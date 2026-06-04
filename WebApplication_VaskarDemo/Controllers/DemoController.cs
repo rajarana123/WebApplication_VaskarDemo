629aa933b704465bb0e52afee56e59de97e20a63
VaskarBanerjee <VaskarBanerjee@IBM-PF3XGKS1>
2026-06-04 18:28:32 +0530

    done

--- WebApplication_VaskarDemo/Controllers/DemoController.cs
+++ WebApplication_VaskarDemo/Controllers/DemoController.cs
@@ -5,7 +5,7 @@ namespace WebApplication_VaskarDemo.Controllers
 {
     public class DemoController : Controller
     {
-        // GET: DemoController item
+        // GET: DemoController item value
         public ActionResult Index()
         {
             return View();
